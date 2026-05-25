7fab5fae6c9bbb87d23234061aeb622729aea02c
hmintopia03 <hmintopia@gmail.com>
2026-05-25 19:13:57 +0900

    redis

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -1,7 +1,12 @@
+import os
 import redis
+from config import REDIS_HOST, REDIS_PORT
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
 
 redis_client = redis.Redis(
-    host="redis",
-    port=6379,
+    host=REDIS_HOST,
+    port=REDIS_PORT,
     decode_responses=True
 )
